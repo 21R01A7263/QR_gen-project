4b10e8d62fad36fa9da40b061acfedaef27efac6
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2023-04-14 17:50:04 +0530

    Cardinal Commit

---
+++ QR Generator/scripts.js
@@ -0,0 +1,85 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 1.4;
+  }, 30);
+}
+
+document.querySelector("button").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 2;
+  }, 30);
+}
+
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 9000, fill: "backwards" });
+}
+
+function genQR()
+        {
+            var gapi="https://chart.googleapis.com/chart?cht=qr&chs=350x350&chl=";
+            var myimg=document.getElementById("myimg");
+            var mytext=document.getElementById("mytext").value;
+
+		        if (mytext!=="")
+            {
+                myimg.src=gapi+mytext+"&chld=l|2";
+		        }
+            else
+            {
+                alert("Please Enter Text");
+            }
+			
+		    
+        }
+		
+
